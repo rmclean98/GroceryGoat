060e496ee5950466022599a14ba526c65811c8bf
fellensteinb17 <fellensteinb17@DESKTOP-M6MMH6O.localdomain>
2021-04-15 13:57:27 -0400

    login functional with 1 user

--- GroceryGoat.py
+++ GroceryGoat.py
@@ -1,10 +1,25 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, session, redirect, url_for
 import os
 
 IMAGE_FOLDER = os.path.join('static', 'images')
 
-app = Flask(__name__)
+class User:
+	def __init__(self, id, firstname, lastname, email, password):
+		self.id = id
+		self.firstname = firstname
+		self.lastname = lastname
+		self.email = email
+		self.password =  password
+	def __repr__(self):
+		return f'<User: {self.firstname} {self.lastname}>'
 
+users = []
+users.append(User(id=1, firstname = 'Brandon', lastname = 'Fellenstein', email = 'fellensteinb17@students.ecu.edu', password = 'TestPass'))
+
+print(users)
+
+app = Flask(__name__)
+app.secret_key = 'tempsecretkey'
 
 app.config['IMAGE_FOLDER'] = IMAGE_FOLDER
 
@@ -23,9 +38,20 @@ def coupons():
 def recipes():
     return render_template('GGRecipe.html')
 
-@app.route('/Login')
+@app.route('/Login', methods=['GET', 'POST'])
 def login():
-    return render_template('GGLogin.html')
+	if request.method == 'POST':
+		session.pop('user_id', None)
+
+		username = request.form['username']
+		password = request.form['password'] 
+
+		user = [x for x in users if x.email == username][0]
+		if user and user.password == password:
+			session['user_id'] = user.id
+			return redirect(url_for('main'))
+		return redirect(url_for('login'))
+	return render_template('GGLogin.html')
 
 @app.route('/Signup')
 def signup():
