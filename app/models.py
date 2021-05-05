fae1e7be3411a05309a69aca6f738afb29c45461
rmclean98 <mcleanry16@students.ecu.edu>
2021-05-05 13:22:13 -0400

    db

--- app/models.py
+++ app/models.py
@@ -1,9 +1,37 @@
 from app import db
-  
+
+class Users(db.Model):
+	__tablename__ = 'Users'
+	userId = db.Column(db.Integer, primary_key=True, nullable=False)    
+	emailId = db.Column(db.String(50), unique = True, nullable=False)    
+	password = db.Column(db.String(16), nullable=False)
+	fname = db.Column(db.String(50), nullable=False)     
+	lname = db.Column(db.String(50), nullable=False) 
+
+
+class ListDetails(db.Model):
+	__tablename__ = 'ListDetails'
+	listId = db.Column(db.Integer, primary_key=True,nullable=False)
+	listTitle = db.Column(db.String(300), nullable=False)
+	userId = db.Column(db.Integer, db.ForeignKey('Users.userId'))
+
+
+class GroceryLists(db.Model):
+	__tablename__ = 'GroceryLists'
+	groceryListsId = db.Column(db.Integer, primary_key=True, nullable=False)
+	listId = db.Column(db.Integer, db.ForeignKey('ListDetails.listId'))
+	userId = db.Column(db.Integer, db.ForeignKey('Users.userId'))
+
+    
 class Todo(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    text = db.Column(db.String(200))
-    complete = db.Column(db.Boolean)
-  
-    def __repr__(self):
-        return self.text
+	__tablename__ = 'Todo'
+	id = db.Column(db.Integer, primary_key=True)
+	listId = db.Column(db.Integer, db.ForeignKey('ListDetails.listId'))
+	text = db.Column(db.String(200))
+	complete = db.Column(db.Boolean)
+
+    
+
+
+
+    
